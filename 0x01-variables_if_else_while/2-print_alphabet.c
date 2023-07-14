dac405776e7edfa8f8034983973d1ae5421ea721
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-14 23:41:20 +0000

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entery poin
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: alwas 0 (success)
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a', i <= z, i++)
+		putchar(i);
+		putchar(\n);
+	return (0);
+}
