c7c07dc7c7643a3494869f40c8de7c27566c833b
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-15 02:18:54 +0000

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,19 +11,19 @@
 
 int main(void)
 {
-	int n1 = 0, n2;
+	int n1 = 48, n2;
 
-	while (n1 < 10)
+	while (n1 <= 56)
 	{
-		n2 = 0;
-		while (n2 < 10)
+		n2 = 49;
+		while (n2 <= 57)
 		{
-			if (n1 != n2 && n1 < n2)
+			if (n1 < n2)
 			{
 				putchar(n1 + 48);
 				putchar(n2 + 48);
 
-				if (n1 + n2 != 17)
+				if (n1 != 56 || n2 != 57)
 				{
 					putchar(',');
 					putchar(' ');
