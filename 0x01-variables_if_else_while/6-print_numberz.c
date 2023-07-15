1f4b9affaa46e5686504abee82704698cb73ecbe
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-15 00:50:38 +0000

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery poin
+ *
+ * Description: numbers from 0 to 9
+ *
+ * Return: alwas 0 (success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar("\n");
+
+	return (0);
+}
