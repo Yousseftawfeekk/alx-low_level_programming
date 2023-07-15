4dd911a55bd7baf299a6518c61d06bacf50c0957
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-15 01:50:08 +0000

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * main - Entery poin
+ *
+ * Description: print all possible diffrent
+ *		combination of all digits
+ *
+ * Return: alwas 0 (success)
+*/
+
+int main(void)
+{
+	int n1 = 0;
+	int n2 = 0;
+
+	while (n1 < 10)
+	{
+		while (n2 < 10)
+		{
+			if (n1 != n2 && n1 < n2)
+			{
+				putchar(n1 + 48);
+				putchar(n2 + 48);
+
+				if (n1 + n2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			n2++;
+		}
+		n1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
+
+
+
+
+
+		putchar(n + 48);
+
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
