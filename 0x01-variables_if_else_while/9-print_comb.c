b8422a0eb0d17ce91b8b74da1416209b01273f91
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-15 01:32:18 +0000

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entery poin
+ *
+ * Description: print numbers from 0 to 9
+ *
+ * Return: alwas 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + 48 );
+		
+		if (n !9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		n++
+	}
+	putchar('\n');
+
+	return (0);
+}
