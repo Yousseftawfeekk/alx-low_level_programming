c0d0c0b2855cdf269626dac5ef320b820748a87c
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-15 00:10:15 +0000

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery poin
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: alwas 0 (success)
+*/
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z' && i != ('e', 'q')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
