61251070c9397f0acc37663dc6d5c1d81557b195
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-15 00:32:49 +0000

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entery poin
+ *
+ * Description: numbers from 0 to 9
+ *
+ * Return: alwas 0 (success)
+*/
+
+int main(void)
+{
+	for (int i = 0 ; i < 10 ; i++);
+	printf("%i");
+
+	return (0);
+}
