ff926853e8cf8b287fa3cfc4e524a2e5379393df
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-15 01:05:43 +0000

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery poin
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: alwas 0 (success)
+*/
+
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
