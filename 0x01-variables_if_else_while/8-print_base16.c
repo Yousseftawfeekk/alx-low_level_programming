750524d240931621af74e06b5184799f66dd5052
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-15 01:17:25 +0000

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entery point
+*
+* Description: print number of base16 in lowercase
+*
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 102 )
+	{
+		putchar(i);
+
+		if (i == 57)
+			i += 39;
+		i++;
+	}
+	putchar('\n')
+
+	return (0);
