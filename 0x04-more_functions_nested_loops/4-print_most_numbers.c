c045d25f75fb6de5a6b8cb12ccc350782a949859
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-21 00:10:22 +0000

    done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers except 2 , 4
+ *
+ * Return: void
+*/
+
+void print_most_numbers(void)
+{
+	char num = '0';
+
+	do
+		_putchar(num);
+
+	while (num <= '9' && num != ('2', '4');
+	_putchar ('\n')
+}
+
