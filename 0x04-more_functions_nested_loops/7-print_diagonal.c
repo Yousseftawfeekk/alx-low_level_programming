f65f7be0055e56490c2bf02e489b84e8658c605d
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-21 01:02:49 +0000

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_diagonal - drwas a diognal line in the terminal
+* @n: number of \ should be printed
+*
+* Return: void
+**/
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++);
+			{
+				if (i == j)
+					_putchar("\\");
+				else if
+					_putchar('');
+			}
+			_putchar('\n');
+		}
+	}
+}
+			
