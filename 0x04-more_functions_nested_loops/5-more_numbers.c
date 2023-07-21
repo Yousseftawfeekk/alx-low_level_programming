78a6a90a3a8dddf637830d3a71c9e8a2948954c9
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-21 00:25:44 +0000

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints number 10 times
+ *		from 0-14
+ *
+ * Return: void
+ *
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for(i = 0; i < 10; i++)
+	{
+		for(j = 0; j < 15; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+} 
