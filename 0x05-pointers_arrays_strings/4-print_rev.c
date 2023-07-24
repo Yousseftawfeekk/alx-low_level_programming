a402a9d82f90cb0831937baaf04ede59f26b80cc
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-24 23:38:47 +0000

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reserved string, followed by new line
+ *
+ * @s: string paramater to print
+ *
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
