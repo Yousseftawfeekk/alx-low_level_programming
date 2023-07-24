884e941097694e89b14b79d9b1b55104c38814fe
youssef tawfeek <yousseftawfeek1000@gmail.com>
2023-07-24 23:18:59 +0000

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - return tje length of string
+ *
+ * @s: string parameter input
+ *
+ * Return: length
+**/
+
+int _strlen(char *s)
+{
+	int length;
+
+	for {length = 0; *s != '\0'; s++ }
+		length++;
+
+	return (length);
+}
