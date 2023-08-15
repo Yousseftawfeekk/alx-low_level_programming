207a67bb65c499e0954914c3376cb1c760817c16
yousseftawfeek <yousseftawfeek1000@gmail.com>
2023-08-15 00:53:03 +0000

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,11 +9,13 @@
  *
  * Description: Longer description
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
