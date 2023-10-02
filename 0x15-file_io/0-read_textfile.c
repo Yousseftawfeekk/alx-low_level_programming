b844fd9b69e4c88215a06de26380022b9df33620
yousseftawfeek <yousseftawfeek1000@gmail.com>
2023-10-02 23:12:21 +0000

    done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include  "main.h"
+
+/**
+ * read_textfile - reads text from file the prints it
+ * @filename: name of file read
+ * @letters: number of bytes to be read
+ *
+ * Return: number of bytes readed and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t fd, bytes, t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buff, letters);
+
+	bytes = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+
+	return (bytes);
+}
